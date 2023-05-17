827001114838fc35d672aa333207ccd295e725d8
Ian Callaghan <50110358+ianshk@users.noreply.github.com>
2023-05-17 08:57:18 +0800

    small title change

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-## React 18, Javascript, Tailwind, Vite, Prettier Base
+## React 18, Javascript, Tailwind, Vite, Prettier
 
 This is my starting point repro for javascript client side applications in React.
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <div className="text-2xl text-slate-600 underline">Hello World!</div>;
 };
 
 export default App;
